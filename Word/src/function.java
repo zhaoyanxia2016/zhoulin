a04653a5d07c3ac6f036df87793ad1c2efa27a8c
zhaoyanxia2016 <48110219+zhaoyanxia2016@users.noreply.github.com>
2019-04-02 13:04:08 +0800

    Update function.java

--- Word/src/function.java
+++ Word/src/function.java
@@ -16,22 +16,27 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 import java.io.IOException;
-public class function { 
+public class function 
+{ 
 	static Scanner in = new Scanner(System.in);
 	Map<String,Integer> Map = new LinkedHashMap<String, Integer>(); 
     //显示词频和柱状图
-    void find(Map<String, Integer> map){
+    void find(Map<String, Integer> map)
+    {
         System.out.println("请输入需要查询的单词 :");
         String string = in.nextLine();
         String[] word= string.split(",");
         float sum;
-        for(int i=0; i<word.length; i++) {
-        	for(Map.Entry<String,Integer> w : map.entrySet()) { 
-        		if(word[i].equals(w.getKey()))
+        for (int i=0; i<word.length; i++)
+	{
+        	for (Map.Entry<String,Integer> w : map.entrySet()) 
+		{ 
+        		if (word[i].equals(w.getKey()))
         		{  
         			System.out.println("单词"+w.getKey() + "出现" + w.getValue()+"次");
         			sum=(float)(w.getValue())/500; 
-        			for(int j=0;j<sum;j++){
+        			for (int j=0;j<sum;j++)
+				{
         				System.out.print("█");
         			}
         			System.out.println();
@@ -40,46 +45,58 @@ public class function {
         }
     }
   //排序 
-    void sort(Map<String, Integer> map,int a) {  
+    void sort(Map<String, Integer> map,int a) 
+    {  
        Set<Entry<String,Integer>> m= map.entrySet();   
        LinkedList<Entry<String, Integer>> List = new LinkedList<Entry<String,Integer>>(m);
-       if(a==2) {
-       Collections.sort(List, new Comparator<Entry<String,Integer>>() {     
-           public int compare(Entry<String, Integer> a,  Entry<String, Integer> b) {  
+       if (a==2) 
+       {
+       Collections.sort(List, new Comparator<Entry<String,Integer>>()
+	{     
+           public int compare(Entry<String, Integer> a,  Entry<String, Integer> b)
+	   {  
                return b.getValue().compareTo(a.getValue());  
           }     
        });  
-       }
-       else if(a==3) {
-    	   Collections.sort(List, new Comparator<Entry<String,Integer>>() {     
-               public int compare(Entry<String, Integer> a,  Entry<String, Integer> b) {  
+     }
+       else if (a==3)
+       {
+    	   Collections.sort(List, new Comparator<Entry<String,Integer>>()
+	{     
+               public int compare(Entry<String, Integer> a,  Entry<String, Integer> b) 
+	       {  
                    return a.getKey().compareTo(b.getKey());  
               }     
            });  
        }
-       for(Entry<String,Integer> entry: List) {  
+       for (Entry<String,Integer> entry: List) 
+       {  
            Map.put(entry.getKey(), entry.getValue());  
        }  
    } 
   //输出前n个单词
-     void print(Map<String, Integer> map) {  
+     void print(Map<String, Integer> map) 
+     {  
     	sort(map，2);
         System.out.println("请输入查看单词个数：");
         int n = in.nextInt();
-        for(Entry<String,Integer> w : Map.entrySet()) {  
+        for (Entry<String,Integer> w : Map.entrySet())
+	{  
             System.out.println("单词"+w.getKey() + "出现" + w.getValue()+"次");  
             n--;
-            if(n<=0)	
+            if (n<=0)	
             	break;
         } 
        
     }  
     //排序写入result.txt
-     void Sort(Map<String, Integer> map)throws IOException {  
+     void Sort(Map<String, Integer> map)throws IOException
+     {  
     	sort(map,3);
         File file = new File("result.txt");
         FileWriter f = new FileWriter(file.getAbsoluteFile());
-        for(Entry<String,Integer> w: Map.entrySet()) {
+        for (Entry<String,Integer> w: Map.entrySet())
+	{
         	f.write(w.getKey() + "/" + w.getValue()+"     ");
         }
         f.close();
